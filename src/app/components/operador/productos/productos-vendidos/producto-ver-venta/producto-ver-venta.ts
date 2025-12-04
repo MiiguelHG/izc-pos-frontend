356fe88e1ea280c413729f0a2066463bfc701312
BrandonEHM <brandonhermart@gmail.com>
2025-12-04 01:24:32 -0600

    tabla de productos vendido, falta modal de detalles

---
+++ src/app/components/operador/productos/productos-vendidos/producto-ver-venta/producto-ver-venta.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-ver-venta',
+  imports: [],
+  templateUrl: './producto-ver-venta.html',
+  styleUrl: './producto-ver-venta.css',
+})
+export class ProductoVerVenta {
+
+}
