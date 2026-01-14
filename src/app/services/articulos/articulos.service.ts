f7d5811f9c12e55e4ba5a88144d6ab415ff79ee2
AlexandreAlmaraz <alexalmarazm14@gmail.com>
2026-01-14 15:02:01 -0600

    Creacion de servicio museo-articulos

--- src/app/services/articulos/articulos.service.ts
+++ src/app/services/articulos/articulos.service.ts
@@ -9,71 +9,46 @@ import { ListaElementos } from '../../interfaces/lista-elementos.interface';
   providedIn: 'root',
 })
 export class ArticulosService {
-
   private http = inject(HttpClient);
+  private API_URL = `${API_CONFIG.baseUrl}${API_CONFIG.endpoints.articulos}`;
 
-  // endpoint general (admin)
-  private ARTICULOS_URL = `${API_CONFIG.baseUrl}${API_CONFIG.endpoints.museoArticulos}`;
-
-  // endpoint relación museo-artículo
-  private MUSEO_ARTICULOS_URL =
-    `${API_CONFIG.baseUrl}/museo-has-articulos`;
-
-  currentPage = signal<number>(1);
+  currentPage = signal<string>('1');
   tipoArticulo = signal<string>('');
-  museoId = signal<number | null>(null);
 
-  //BOLETOS (admin)
   private boletoEstandarResource = httpResource<Response<Articulo[] | null>>(() => ({
-    url: `${this.ARTICULOS_URL}/tipo/boleto`,
+    url: `${this.API_URL}/tipo/boleto`,
+  }));
+
+  private articulosResource = httpResource<Response<ListaElementos<Articulo> | null>>(() => ({
+    url: this.API_URL,
+    params: {
+      page: this.currentPage(),
+      tipo: this.tipoArticulo(),
+    },
   }));
 
   readonly boletoEstandar = this.boletoEstandarResource.asReadonly();
-
-  /*ARTÍCULOS (admin)*/
-  private articulosResource =
-    httpResource<Response<ListaElementos<Articulo> | null>>(() => ({
-      url: this.ARTICULOS_URL,
-      params: {
-        page: this.currentPage(),
-        tipo: this.tipoArticulo(),
-      },
-    }));
-
   readonly articulos = this.articulosResource.asReadonly();
 
-  //PRODUCTOS (OPERADOR)
-  private productosResource =
-    httpResource<Response<any>>(() => {
-      const museoId = this.museoId();
-      if (!museoId) return;
-
-      return {
-        url: `${this.MUSEO_ARTICULOS_URL}/museo/${museoId}/articulos/producto`,
-        params: {
-          page: this.currentPage(),
-        },
-      };
-    });
-
-  readonly productos = this.productosResource.asReadonly();
-
-  recargarProductos() {
-    this.productosResource.reload();
-  }
-
-  //CRUD ADMIN
   createArticulo(articulo: Articulo) {
-    this.http.post<Response<Articulo>>(this.ARTICULOS_URL, articulo).subscribe({
-      next: () => this.articulosResource.reload(),
-      error: (err) => console.error('❌ Error al crear artículo', err.error),
+    this.http.post<Response<Articulo>>(`${this.API_URL}`, articulo).subscribe({
+      next: (res) => {
+        this.articulosResource.reload();
+      },
+      error: (err) => {
+        console.error('❌ Error al crear el artículo:', err.error);
+      }
     });
   }
 
   editArticulo(articuloId: number, articulo: Articulo) {
-    this.http.put(`${this.ARTICULOS_URL}/${articuloId}`, articulo).subscribe({
-      next: () => this.articulosResource.reload(),
-      error: (err) => console.error('❌ Error al editar artículo', err.error),
+    this.http.put<Response<Boolean>>(`${this.API_URL}/${articuloId}`, articulo).subscribe({
+      next: (res) => {
+        this.articulosResource.reload();
+      },
+      error: (err) => {
+        console.error('❌ Error al editar el artículo:', err.error);
+      }
     });
   }
 }
