6f63f7eadc2c1d0af513c91dd05038378ba48b20
BrandonEHM <brandonhermart@gmail.com>
2026-03-23 16:55:09 -0600

    Footer

---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,38 @@
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+})
+export class Footer {
+
+  isOpen: boolean = false;
+
+  togglePanel(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const footerEl = target.closest('footer');
+    if (!footerEl && this.isOpen) {
+      this.isOpen = false;
+    }
+  }
+
+  @HostListener('document:keydown.escape')
+  onEscape(): void {
+    if (this.isOpen) this.isOpen = false;
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+
+}
